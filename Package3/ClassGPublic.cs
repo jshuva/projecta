cb64bb88199b8afea703a408ab6288264a9a829a
jshuva <jayanta.shuva@gmail.com>
2015-01-06 17:01:13 +0600

    DIP and DI Done

---
+++ Package3/ClassGPublic.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Package3
+{
+    public class ClassGPublic : IInterfaceG
+    {
+        ClassG g = new ClassG();
+
+        public void Method1()
+        {
+            g.Method1();
+        }
+    }
+}
